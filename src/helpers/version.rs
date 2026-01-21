6a53081605afa3b1bf617a326d7cb52316794da3
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-01-21 23:40:21 +0200

    feat(version): allow custom version overwrite during the build

--- src/helpers/version.rs
+++ src/helpers/version.rs
@@ -1,1 +1,5 @@
-pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+// pub const VERSION: &str = option_env!("GUITAR_BUILD_OVERWRITE_VERSION").unwrap_or(env!("CARGO_PKG_VERSION")); // require unstable for now
+pub const VERSION: &str = match option_env!("GUITAR_BUILD_OVERWRITE_VERSION") {
+    Some(v) => v,
+    None => env!("CARGO_PKG_VERSION"),
+};
